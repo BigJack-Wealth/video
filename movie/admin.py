03e4f6ddde4d1796ce202c9cfa9ef57fc3d53ea1
BigJack-Wealth <88883126+BigJack-Wealth@users.noreply.github.com>
2022-05-10 16:41:58 +0100

    hi

---
+++ movie/admin.py
@@ -0,0 +1,32 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Koati, Video
+from .models import VideoD, Uncharted, Batman, Matrix, Morbius, Moonfall, TIB, Turn, Koati, OldGuard, Dune, X, Ecanto, Cellar
+from .models import Riverdance, YesDay, Stowaway, Vivo
+# from .models import Movie
+
+
+
+admin.site.register(Video)
+admin.site.register(VideoD)
+admin.site.register(Uncharted)
+admin.site.register(Batman)
+admin.site.register(Matrix)
+admin.site.register(Morbius)
+admin.site.register(Moonfall)
+admin.site.register(TIB)
+admin.site.register(Turn)
+admin.site.register(Koati)
+admin.site.register(OldGuard)
+admin.site.register(Dune)
+admin.site.register(X)
+admin.site.register(Ecanto)
+admin.site.register(Cellar)
+admin.site.register(Riverdance)
+admin.site.register(YesDay)
+admin.site.register(Stowaway)
+admin.site.register(Vivo)
+
+
+

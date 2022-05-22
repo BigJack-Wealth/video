33a4aca11029c5cde750c833521b08ac4df65be6
BigJack-Wealth <88883126+BigJack-Wealth@users.noreply.github.com>
2022-05-22 11:55:20 +0100

    recent

--- crm1/settings.py
+++ crm1/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-d+j7gw$&6r-2&c3@5w97k$(e(p7-9w3kl9rkq8s^xr2j(p(nzm
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['54.147.142.89','*']
 
@@ -135,7 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'crm1/static')
 ]
@@ -143,8 +143,8 @@ STATICFILES_DIRS = [
 #                 os.path.join(BASE_DIR, 'movie/static')
 # ]
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'crm1/static/images/uploads')
+MEDIA_URL = "/images/"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'crm1/static/images')
 
 
 LOGIN_REDIRECT_URL ='/'
